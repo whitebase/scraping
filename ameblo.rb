430145807bb2ee73a5e2902aa5ae022323a50cb9
gen <gen@emzah.com>
2013-03-16 20:45:11 +0900

    微修正

--- ameblo.rb
+++ ameblo.rb
@@ -16,73 +16,66 @@ def contens(page)
   body += title
   body += date_str
   contents.split.each do | str |
-        if str == '<!--'
-          break
-        end
-      body += str.to_s
+    if str == '<!--'
+      break
+    end
+  body += str.to_s
   end
   return body
 end
 
-
+body = ''
 if ARGV[0]
   ameblo_id =  ARGV[0]
   a = Mechanize.new
   uri = "http://ameblo.jp/#{ameblo_id}/"
   count = 0
   a.get(uri) do |ameblo_page|
-    
     page_count = 1
-    
     paging_links = ameblo_page.at('div.topPaging').search('a')
     last_page_link =  paging_links.find_all{|e| e['class'] == 'lastPage'}.map{|e| e['href'] }
     last_page_num = last_page_link[0].to_s.split('/').last.split('-')[1].to_s.split('.')[0].to_i
-    
     i = 0
     while i < last_page_num
       uri =  "http://ameblo.jp/#{ameblo_id}/page-" + i.to_s + ".html"
       sleep(1)
       a.get(uri) do  |page|
-       #contens(page)
-       date = page.at('span.date').inner_text
-       time = Time.parse date
-       date_str = time.year.to_s + "/" + time.month.to_s + "/" + time.day.to_s + " " + time.hour.to_s + ":"+time.min.to_s + "\n" 
-=begin
-	   contents = page.at('div.contents').inner_text
-	   body = ""
-	   body += page.at('h3//a').inner_text
-	   body += date_str
-	   contents.split.each do | str |
-	        if str == '<!--'
-	          break
-	        end
-	        body += str.to_s
-	   end
-=end
-		imgs = page.at('div.contents').search('img').find_all{|e| e['src'] =~ /(?:.*jpg)/i && e['width'] == nil }.map{|e| e['src']}  
-		f = 0
-		imgs.each do | e |
-			paths = e.split('/')
-			  o = "o" + paths.last.split('_').last
-			file = e.to_s.sub(paths.last,o)
-			a.get(file).save
-			body += file + ','
-			#savefile = "/User/miruku/dir/#{ameblo_id}-#{f}.jpg"
-			#p savefile
-			#a.get(file).save_as(savefile) #オリジナル画像保存   
-			f += 1
-		end
-	    i+=1
-=begin       
-      file_name = "#{ameblo_id}.txt"
-      File.open(file_name, 'a') {|file|
-	       file.write(body)
-	    }
+        #contens(page)
+        date = page.at('span.date').inner_text
+        time = Time.parse date
+        date_str = time.year.to_s + "/" + time.month.to_s + "/" + time.day.to_s + " " + time.hour.to_s + ":"+time.min.to_s + "\n" 
+
+        contents = page.at('div.contents').inner_text
+
+        body += page.at('h3//a').inner_text
+        body += date_str
+        contents.split.each do | str |
+          if str == '<!--'
+            break
+          end
+        body += str.to_s
+        end
+        imgs = page.at('div.contents').search('img').find_all{|e| e['src'] =~ /(?:.*jpg)/i && e['width'] == nil }.map{|e| e['src']}  
+        f = 0
+        imgs.each do | e |
+          paths = e.split('/')
+        o = "o" + paths.last.split('_').last
+        file = e.to_s.sub(paths.last,o)
+        a.get(file).save
+        body += file + ','
+        #savefile = "/User/miruku/dir/#{ameblo_id}-#{f}.jpg"
+        #p savefile
+        #a.get(file).save_as(savefile) #オリジナル画像保存   
+        f += 1
+        end
+        i+=1
       end
-=end
-      
-    end
-  end
-end
+    end 
+  end #end while     
+  file_name = "#{ameblo_id}.txt"
+  File.open(file_name, 'a') {|file|
+    file.write(body)
+  }
+end #end if
 =begin
 =end
